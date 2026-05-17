5cf32e1f14a1779cedcaa8736efe6fa1e7239fed
Archita Das <144708429+archita050206@users.noreply.github.com>
2026-05-17 12:06:54 +0530

    script.js has been updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express()
 const PORT= process.env.PORT ?? 8080
 
 app.get('/', (req,res)=>{
-    return res.json({msg: "Hello from the server"})
+    return res.json({msg: "Hello from the server, this message has been updated"})
 })
 app.listen(PORT, ()=>{
     console.log(`Server up and running on port: ${PORT}`)
